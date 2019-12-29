a04f4db7ad6bae81bf1211b3a27dde4317c51ad9
DomenicoLibri <domenico.libri01@gmail.com>
2019-12-29 14:28:50 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import TodoItems from "./TodoItems";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<TodoItems />, document.getElementById('root'));
+
+
